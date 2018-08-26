20317022d89564c587a2aff41e81136236e409b2
Ismail Jalloh <ijalloh21@gmail.com>
2018-08-26 23:05:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function takeANumber(cLine,name) {
+  cLine.push(name)
+  return "Welcome, " + name + ". You are number " + cLine.length + " in line."
+}
+
+function nowServing(katzDeliLine) {
+  while (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine[0] + "." 
+    katzDeliLine = katzDeliLine.splice(0,1);
+  }
+  while (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+}
